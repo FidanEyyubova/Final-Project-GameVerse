558713e9cda2d1fe969eefd46be545117ed7ea69
Fidan-Eyyubova <fidaneyybva0@gmail.com>
2025-02-02 17:59:01 +0400

    navbar and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { FaRegUser } from "react-icons/fa";
+import { FiMoon } from "react-icons/fi";
+import { MdLanguage } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="head d-flex justify-content-center align-items-center text-center">
+      <div className="container-fluid">
+        <div className="row w-100 my-2 d-flex justify-content-between align-items-center">
+          <div className="col-lg-6 col-md-6 col-6 d-flex justify-content-start align-items-center">
+            <div className="mx-3">
+              <img src="../src/images/logo.png" alt="" className="img-logo" />
+            </div>
+            <button type="button" className=" moon  mx-3">
+              <FiMoon />
+            </button>
+          </div>
+
+          <div className="col-lg-6 col-md-6 col-6 d-flex justify-content-end align-items-center">
+            <div className="dropdown-center mx-3">
+              <button
+                className="dropdown-toggle"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img src="https://www.svgrepo.com/show/405643/flag-for-flag-united-kingdom.svg" alt="" className="img-lang" />
+                <small className="mx-1">ENG</small>
+              </button>
+              <ul className="dropdown-menu  custom-dropdown-menu text-center">
+                <li>
+                  <a className="dropdown-item" href="#">
+                  <img
+                      src="https://www.svgrepo.com/show/405416/flag-for-flag-azerbaijan.svg"
+                      alt=""
+                      className="img-lang"
+                    />
+                    <small className="mx-2">AZ</small>
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                  <img
+                      src="https://www.svgrepo.com/show/405590/flag-for-flag-russia.svg"
+                      alt=""
+                      className="img-lang"
+                    />
+                    <small className="mx-1">RUS</small>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <Link  className="user">
+                <FaRegUser />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { BsSearch } from "react-icons/bs";
+import { FaRegHeart } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [openModal, setOpenModal] = useState();
+
+  const handleOpenModal = () => {
+    setOpenModal(!openModal);
+  };
+
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-dark py-4"
+      aria-label="Fifth navbar example"
+    >
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarsExample05"
+          aria-controls="navbarsExample05"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarsExample05">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-4 mx-3 px-3">
+            <Link className="page mx-2">Discover</Link>
+            <Link className="page mx-2">Games</Link>
+            <Link className="page mx-2">About</Link>
+            <Link className="page mx-2">Blog</Link>
+            <Link className="page mx-2">Contact us</Link>
+          </ul>
+          <div className="icons d-flex gap-4 mx-4">
+            <div className="icon">
+              <Link className="icon"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                <BsSearch onClick={handleOpenModal} />
+              </Link>
+            </div>
+            <div>
+              <button type="button" className="position-relative icon">
+                <FaRegHeart />
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill">
+                  0<span className="visually-hidden">unread messages</span>
+                </span>
+              </button>
+            </div>
+            <div>
+              <button type="button" className="position-relative icon">
+                <FiShoppingCart />
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill">
+                  0<span className="visually-hidden">unread messages</span>
+                </span>
+              </button>
+            </div>
+          </div>
+          {openModal && (
+            <div
+              className="modal fade"
+              id="exampleModal"
+              tabIndex={-1}
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="exampleModalLabel">
+                      Search for products
+                    </h1>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    />
+                  </div>
+                  <div className="modal-body">
+                    <form action="" className="text-center">
+                    <input type="text" className="search w-100  form-control rounded-3 border-1 shadow-sm"
+                    placeholder="Search" />
+
+                    </form>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
