4f1e245bc9fe68dd0fb1ba4bddc84eeb8af6ea4d
Fidan-Eyyubova <fidaneyybva0@gmail.com>
2025-02-02 23:22:44 +0400

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
@@ -5,6 +6,7 @@ function App() {
   return <>
   <Header />
   <Navbar />
+  <Footer />
   </>;
 }
 
